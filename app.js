6621c4ea723173589271490c88e9d2309b4e07e8
edthuman <edwardadirvine@aol.com>
2024-02-20 14:04:35 +0000

    add files, functions, and tests for POST /api/articles/:article_id/comments endpoint functionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const { getTopics } = require("./controllers/topics.controllers")
 const { getEndpoints } = require("./controllers/api.controllers")
-const { getArticleById, getArticles, getCommentsByArticle } = require("./controllers/articles.controllers")
+const { getArticleById, getArticles, getCommentsByArticle, postComment } = require("./controllers/articles.controllers")
 const app = express()
+
+app.use(express.json())
 
 app.get("/api", getEndpoints)
 
@@ -13,6 +15,8 @@ app.get("/api/articles", getArticles)
 app.get("/api/articles/:article_id", getArticleById)
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticle)
+
+app.post("/api/articles/:article_id/comments", postComment)
 
 app.use("/*", (request, response, next) => {
     response.status(404).send({ msg: "Not found" })
@@ -27,22 +31,18 @@ app.use((error, request, response, next) => {
 })
 
 app.use((error, request, response, next) => {
-    if (error.code === "22P02") {
-        response.status(400).send({ msg: "Bad request"})
+    if (error.code === "22P02" || error.code === "23502") {
+        response.status(400).send({ msg: "Bad request" })
+    } else  if (error.code === "23503") {
+        response.status(404).send({ msg: "Not found" })
     } else {
         next(error)
     }
 })
 
 app.use((error, request, response, next) => {
-    
+    console.log(error)
+    response.status(500).send({ msg: "Internal server error"})
 })
 
 module.exports = app
-
-/*
-Consider errors 
-test for them.
-
-Remember to add a description of this endpoint to your /api endpoint. 
-*/
