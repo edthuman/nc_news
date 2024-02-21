89c6d911e91e5c088c250f3c96f9aacb14029249
edthuman <edwardadirvine@aol.com>
2024-02-21 08:47:27 +0000

    add functions and tests for topic query on GET /articles endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,15 +1,24 @@
 const db = require("../db/connection");
 
-exports.selectAllArticles = () => {
+exports.selectAllArticles = (topic) => {
+    const validTopics = ["mitch", "cats", "paper"]
+    let topicQuery = ""
+
+    if (validTopics.includes(topic)) {
+        topicQuery += `WHERE topic = '${topic}'`
+    } else if (topic !== undefined) {
+        return Promise.reject({ status: 404, msg: "Not found"})
+    }
+
     return db
-        .query(
-            `
-    SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, CAST (COUNT(comment_id) AS INTEGER) AS comment_count FROM articles
-    LEFT JOIN comments ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id
-    ORDER BY articles.created_at DESC;
-    `
-        )
+        .query(`
+            SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, CAST (COUNT(comment_id) AS INTEGER) AS comment_count FROM articles
+            LEFT JOIN comments ON articles.article_id = comments.article_id
+            ${topicQuery}
+            GROUP BY articles.article_id
+            ORDER BY articles.created_at DESC
+            ;
+        `)
         .then(({ rows }) => {
             return rows;
         });
