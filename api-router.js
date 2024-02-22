bd347c5127c9d12c113debefe184ffbcb8056e22
edthuman <edwardadirvine@aol.com>
2024-02-22 14:31:34 +0000

    add router for users endpoint

--- api-router.js
+++ api-router.js
@@ -3,8 +3,10 @@ const commentsRouter = require("./comments-router");
 const { getEndpoints } = require("./controllers/api.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
 const { getUsers } = require("./controllers/users.controllers");
+const usersRouter = require("./users-router");
 
 const apiRouter = require("express").Router();
+
 
 apiRouter.get("/", getEndpoints);
 
@@ -14,6 +16,7 @@ apiRouter.use("/articles", articlesRouter)
 
 apiRouter.use("/comments", commentsRouter);
 
-apiRouter.get("/users", getUsers);
+apiRouter.use("/users", usersRouter);
+
 
 module.exports = apiRouter;
